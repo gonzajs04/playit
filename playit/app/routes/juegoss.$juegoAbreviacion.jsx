b87e01075ef77ea2c68f2b40926554f037e82a99
Gonzalo <hernandezgonzalo5845@gmail.com>
2023-08-23 18:44:02 -0300

    Verification data api

--- playit/app/routes/juegoss.$juegoAbreviacion.jsx
+++ playit/app/routes/juegoss.$juegoAbreviacion.jsx
@@ -5,6 +5,7 @@ import styles from "../styles/juegos.css";
 import { ABR } from '../helpers/helpers.js'
 import Typing from "../components/Typing.jsx";
 import Tateti from "../components/Tateti.jsx";
+import { useEffect } from "react";
 
 export async function loader({ params }) {
     const words = await getPalabras();
@@ -16,16 +17,21 @@ export function links() {
     return [{ rel: "stylesheet", href: styles }];
   }
 export default function Juego() {
+  
     const data = useLoaderData();
-    return (
-        <>
-            {data[0] === ABR.TY ? (
-                <Typing
-                     data={data}
-                />
-            ):(
-                <Tateti/>
-            )}
-        </>
-    );
+
+    if(data?.length>0){
+        return (
+            <>
+                {data[0] === ABR.TY ? (
+                    <Typing
+                         data={data}
+                    />
+                ):(
+                    <Tateti/>
+                )}
+            </>
+        );
+    }
+   
 }
